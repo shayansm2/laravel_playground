5f6bf57c941789d58613cd5883a9e7582320abb8
shayansm <s.shafieemoghadam@digikala.com>
2022-11-10 21:40:53 +0330

    add this to help me use better of the model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
+
+/**
+ * @method static self find(int $id)
+ */
 
 class User extends Authenticatable
 {
