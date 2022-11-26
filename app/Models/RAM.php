af4b746fd59201512b2db128f01bba8ab55f65fa
shayansm <s.shafieemoghadam@digikala.com>
2022-11-26 08:58:56 +0330

    another way of defining accessors

--- app/Models/RAM.php
+++ app/Models/RAM.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,13 +17,10 @@ class RAM extends Model
         'capacity',
     ];
 
-    public function getPriceAttribute()
+    public function price(): Attribute
     {
-        return $this->getCapacity() * 20;
-    }
-
-    private function getCapacity()
-    {
-        return $this->capacity;
+        return Attribute::get(
+            fn ($value, $attributes) => $attributes['capacity'] * 20,
+        );
     }
 }
